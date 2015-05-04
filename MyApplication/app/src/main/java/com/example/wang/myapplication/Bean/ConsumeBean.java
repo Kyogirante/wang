f27918ac42db8428f4df61969e06fd7d07715322
wangkyo <wangkegang_hit@163.com>
2015-05-04 20:02:07 +0800

    add consume bean

---
+++ MyApplication/app/src/main/java/com/example/wang/myapplication/Bean/ConsumeBean.java
@@ -0,0 +1,69 @@
+package com.example.wang.myapplication.Bean;
+
+import java.util.Date;
+
+/**
+ * Created by haha201 on 2015/5/4.
+ */
+public class ConsumeBean {
+
+
+    private double cost;
+    private String category;
+    private String describe_msg;
+    private Date time;
+
+    public ConsumeBean(){
+
+    }
+
+    public ConsumeBean(double cost, String category, String describe_msg, Date time) {
+        this.cost = cost;
+        this.category = category;
+        this.describe_msg = describe_msg;
+        this.time = time;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public String getDescribe_msg() {
+        return describe_msg;
+    }
+
+    public void setDescribe_msg(String describe_msg) {
+        this.describe_msg = describe_msg;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "ConsumeBean{" +
+                "cost=" + cost +
+                ", category='" + category + '\'' +
+                ", describe_msg='" + describe_msg + '\'' +
+                ", time=" + time +
+                '}';
+    }
+
+}
