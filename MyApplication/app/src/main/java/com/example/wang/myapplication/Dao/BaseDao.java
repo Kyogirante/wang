7dc61d523bf63f26e7a74be3c58548730e426ba9
wangkyo <wangkegang_hit@163.com>
2015-05-05 18:19:24 +0800

    modify dao

--- MyApplication/app/src/main/java/com/example/wang/myapplication/Dao/BaseDao.java
+++ MyApplication/app/src/main/java/com/example/wang/myapplication/Dao/BaseDao.java
@@ -1,22 +1,40 @@
 package com.example.wang.myapplication.Dao;
 
+import android.database.Cursor;
+
 /**
  * Created by wang on 2015/5/4.
  */
 public abstract class BaseDao {
-    public int getIntegerData(){
-        return 0;
+
+    protected static int getColumnIndex(Cursor cursor, String columnName){
+        return cursor.getColumnIndex(columnName);
     }
 
-    public String getStringData(){
+    public static int getIntegerData(Cursor cursor, String columnName, int defaultValue){
+        return getIntegerData(cursor,getColumnIndex(cursor,columnName),defaultValue);
+    }
+
+    public static int getIntegerData(Cursor cursor,int index, int defaultValue){
+        try{
+            if(cursor.isNull(index)){
+                return defaultValue;
+            }else {
+                return cursor.getInt(index);
+            }
+        } catch (Exception e){
+            return defaultValue;
+        }
+    }
+    public static String getStringData(){
         return null;
     }
 
-    public double getDoubleData(){
+    public static double getDoubleData(){
         return 0f;
     }
 
-    public boolean getBooleanData(){
+    public static boolean getBooleanData(){
         return false;
     }
 
